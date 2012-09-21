a380967201c8f9afd2641544289309c3c3d516bb
johnoly99 <johnoly99@gmail.com>
2012-09-21 13:34:49 -0400

    adjust some steps per and home offset position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -210,7 +210,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 // For Rostock this means top and center of the cartesian print volume.
 #define X_HOME_POS 0
 #define Y_HOME_POS 0
-#define Z_HOME_POS 353 // Distance between nozzle and print surface after homing.
+#define Z_HOME_POS 353.5 // Distance between nozzle and print surface after homing.
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
@@ -218,7 +218,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 
 // default settings 
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {53.333, 53.333, 53.333, 284.0}  // default steps per unit for rostockmax
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {53.333, 53.333, 53.333, 292.0}  // default steps per unit for rostockmax
 #define DEFAULT_MAX_FEEDRATE          {300, 300, 300, 300}  // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000, 9000, 9000, 9000}  // X, Y, Z, E maximum start speed for accelerated moves.
 
